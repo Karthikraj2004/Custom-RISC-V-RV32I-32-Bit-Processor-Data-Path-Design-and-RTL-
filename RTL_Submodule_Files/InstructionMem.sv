d14b9e1a3d699d9346edefb90d0d3e3a43b93c5c
KRM <95900351+Karthikraj2004@users.noreply.github.com>
2026-09-17 20:21:13 -0700

    InstructionMem Write Enable

--- RTL_Submodule_Files/InstructionMem.sv
+++ RTL_Submodule_Files/InstructionMem.sv
@@ -1,9 +1,17 @@
 module InstructionMem (
     input logic [31:0] pc,
+    input logic        imem_write_enable;
+    input logic [4:0]  imem_write_addr;
+    input logic [31:0] imem_write_data;
     output logic [31:0] instruction
 );
 
     logic [31:0] mem[0:31];
+
+    always_ff @(posedge clk) begin
+        if (imem_write_enable)
+            mem[imem_write_addr] <= imem_write_data;
+    end
 
     assign instruction = mem[pc[6:2]]; // Assuming word-aligned addresses
 
